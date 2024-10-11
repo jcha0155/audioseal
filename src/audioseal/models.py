e768fb267573a29d5288d8e9f2b64d180c52e0b1
jcha0155 <jcha0155@student.monash.edu>
2024-10-11 05:16:35 +0000

    Pushing an update

--- src/audioseal/models.py
+++ src/audioseal/models.py
@@ -77,6 +77,9 @@ class AudioSealWM(torch.nn.Module):
     def message(self, message: torch.Tensor) -> None:
         self._message = message
 
+    def get_original_payload(self) -> Optional[torch.Tensor]:
+        return self._original_payload  # Method to retrieve the stored payload Step 2: Use the Modified Class
+        
     def get_watermark(self, x: torch.Tensor, sample_rate: Optional[int] = None, message: Optional[torch.Tensor] = None) -> torch.Tensor:
         length = x.size(-1)
         if sample_rate is None:
@@ -111,9 +114,6 @@ class AudioSealWM(torch.nn.Module):
             watermark = torch.tensor(resampled_watermark, device=watermark.device)
 
         return watermark[..., :length]
-
-    def get_original_payload(self) -> Optional[torch.Tensor]:
-        return self._original_payload  # Method to retrieve the stored payload Step 2: Use the Modified Class
 
     def forward(self, x: torch.Tensor, sample_rate: Optional[int] = None, message: Optional[torch.Tensor] = None,
                 n_fft: int = 2048, hop_length: int = 512, min_alpha: float = 0.5, max_alpha: float = 1.5) -> torch.Tensor:
